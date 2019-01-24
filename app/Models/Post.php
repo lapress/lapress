c24789a91a9b84057055671b4ddf5202c55c1581
Sebastian Szczepanski <sebastian@ably.pl>
2019-01-24 23:01:25 +0100

    integrate tnt search

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,42 @@ namespace App\Models;
 
 use LaPress\Models\Post as BasePost;
 
+use Laravel\Scout\Searchable;
+
 /**
  * @author    Sebastian Szczepański
  * @copyright ably
  */
 class Post extends BasePost
 {
+    use Searchable;
+
+    public $asYouType = true;
+
+    /**
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        if ($this->isDraft()) {
+            return [];
+        }
+
+        return [
+            'ID'         => $this->ID,
+            'post_title' => $this->post_title,
+            'body'       => $this->post_content,
+            'categories' => $this->categories->pluck('name')->implode(', '),
+            'post_date'  => $this->date,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'promotions';
+    }
+
 }
